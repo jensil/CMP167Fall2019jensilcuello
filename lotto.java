6adfa31951a5f02af9c7504c864cb707c3c39354
jensil <54601101+jensil@users.noreply.github.com>
2019-09-03 16:53:08 -0400

    Add files via upload

---
+++ lotto.java
@@ -0,0 +1,29 @@
+package megamillion;
+import java.util.Scanner;
+public class lotto {
+  public static void main(String[] args) {
+	Scanner number = new Scanner(System.in);
+	//new thing to use  instead of scanner is 
+	//Random randGen = new Random();
+	//System.out.println(randGen.nextInt();
+	//int rnum1=randGen.nextInt(60) + 1; the 1 makes the pc to use any number except 0
+	/* 1-declare variable to hold amount of dollar
+	 * 2-declare variable for cost of ticket
+	 * 3-prompt user for amount of dollars
+	 * 4-store amount into amount of dollar variable
+	 * 5-ask to input 6 numbers
+	 * 6-create variable for each number
+	 * 7-randomly generate six number
+	 */
+	 
+	   int dollars;
+	   int numbers;
+	   
+System.out.println("Enter the amount of dollars you want to play: ");
+   
+System.out.println("Enter six numbers: ");
+
+System.out.println("Your numbers are:");
+System.out.println(number);
+}
+}
