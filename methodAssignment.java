e6a753dcc14f333f1a4cbda662fdf13f5bd7a3fc
jensil <54601101+jensil@users.noreply.github.com>
2019-10-10 16:43:33 -0400

    Add files via upload

---
+++ methodAssignment.java
@@ -0,0 +1,18 @@
+package week6;
+
+public class methodAssignment {
+	public static int productthree( int num1, int num2, int num3) {
+ return num1 * num2 * num3;
+}
+public static int sumbetween(int num1, int num2, int i) {
+ 
+	for  (i = num1; i <=num2; i++) {
+}
+	 return (i);  
+  
+}
+}
+
+
+
+
